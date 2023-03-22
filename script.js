46b7645d46e26248ff94b2d415b45c9d67842c85
arpanaditya <adityaarpansahoo@gmail.com>
2023-03-23 01:03:32 +0530

    feat: implement form validation

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-circle = document.querySelector('.circle');
-active = document.querySelector('.active');
-circle.classList.add('') //adds active class
+// circle = document.querySelector('.circle');
+// active = document.querySelector('.active');
+// circle.classList.add('') //adds active class
+
+const btn = document.querySelector(".next-step");
+const input = document.querySelectorAll("input");
+const error = document.querySelectorAll(".error");
+
+const validate = () => {
+    input.forEach((input) => {
+        if (input.value === "") {
+            error.forEach((err) => {
+                err.style.display = "block";
+                input.style.border = "2px solid red";
+            })
+        }
+    })
+}
+
+btn.addEventListener("click", validate);
